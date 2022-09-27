11a5e01656c4f961e14f4a208458e7e2628c4583
frsh-prnc <pedro.vieira1909@gmail.com>
2022-09-27 15:37:53 -0300

    primeiro commit

---
+++ mobile/src/utils/games.ts
@@ -0,0 +1,38 @@
+export const GAMES = [
+  {
+    id: 'd7c9763c-e916-4f5d-a92d-8e94610b8904',
+    name: 'League of Legends',
+    ads: '4',
+    cover: require('../assets/games/game-1.png')
+  },
+  {
+    id: '2',
+    name: 'Dota 2',
+    ads: '3',
+    cover: require('../assets/games/game-2.png')
+  },
+  {
+    id: '3',
+    name: 'CS-GO',
+    ads: '2',
+    cover: require('../assets/games/game-3.png')
+  },
+  {
+    id: '4',
+    name: 'Apex',
+    ads: '3',
+    cover: require('../assets/games/game-4.png')
+  },
+  {
+    id: '5',
+    name: 'Fortnite',
+    ads: '5',
+    cover: require('../assets/games/game-5.png')
+  },
+  {
+    id: '6',
+    name: 'World of Warcraft',
+    ads: '2',
+    cover: require('../assets/games/game-6.png')
+  },
+];
---
+++ web/src/App.tsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { CreateAdBanner } from './assets/components/CreateAdBanner';
+import { GameBanner } from './assets/components/GameBanner';
+import Axios from "axios";
+
+
+import './styles/main.css';
+
+import logoImg from './assets/logo-nlw-esports.svg';
+import { CreateAdModal } from './assets/components/CreateAdModal';
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+
+}
+
+function App() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => { 
+  Axios('http://localhost:3333/games').then(response => {
+    setGames(response.data)
+  })
+}, [])
+
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="logo" />
+
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui!
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        {games.map(game =>{
+          return (
+            <GameBanner 
+            key={game.id}
+            title={game.title} 
+            bannerUrl={game.bannerUrl} 
+            adsCount={game._count.ads}
+            />
+          )
+        })}
+      </div>
+
+      <Dialog.Root>
+        <CreateAdBanner />
+        <CreateAdModal/>
+      </Dialog.Root>
+    </div>
+  );
+}
+
+export default App;
+function axios(arg0: string) {
+  throw new Error('Function not implemented.');
+}
+
